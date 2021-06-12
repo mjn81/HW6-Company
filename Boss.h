74be2d010d801eb5d0699c5e84647a06396e68be
mjn81 <49482065+mjn81@users.noreply.github.com>
2021-06-13 00:08:14 +0430

    Boss created

---
+++ Boss.h
@@ -0,0 +1,20 @@
+//
+// Created by Webhouse on 6/12/2021.
+//
+
+#ifndef HW6_2_BOSS_H
+#define HW6_2_BOSS_H
+
+#include "Employee.h"
+
+class Boss : public Employee{
+private:
+    int numberOfEmployees;
+public:
+
+
+
+};
+
+
+#endif //HW6_2_BOSS_H
