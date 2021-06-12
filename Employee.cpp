0b55556beb931dfea663e6b56af4cd19cd68430d
mjn81 <49482065+mjn81@users.noreply.github.com>
2021-06-12 21:06:54 +0430

    Employee Added

---
+++ Employee.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Webhouse on 6/12/2021.
+//
+
+#include "Employee.h"
---
+++ Employee.h
@@ -0,0 +1,11 @@
+
+#ifndef HW6_2_EMPLOYEE_H
+#define HW6_2_EMPLOYEE_H
+
+
+class Employee {
+
+};
+
+
+#endif //HW6_2_EMPLOYEE_H
