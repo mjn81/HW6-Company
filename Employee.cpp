e6577da45203f3fc89ecc926b2faa75348cfb9e7
mjn81 <49482065+mjn81@users.noreply.github.com>
2021-06-12 22:32:17 +0430

    Employee getter/setter , = , cons & copy cons completed

--- Employee.cpp
+++ Employee.cpp
@@ -1,5 +1,63 @@
-//
-// Created by Webhouse on 6/12/2021.
-//
+
 
 #include "Employee.h"
+
+Employee::Employee(std::string id, std::string name, Address add, int hw, int sph, int wtd, int wd)
+: Person(id , name , add) {
+    hourWork = hw;
+    salaryPerHour = sph;
+    workToDo = wtd;
+    workDone = wd;
+}
+
+Employee::Employee(const Employee &cp) : Person(cp) {
+    hourWork = cp.hourWork;
+    salaryPerHour = cp.salaryPerHour;
+    workToDo = cp.workToDo;
+    workDone = cp.workDone;
+}
+
+Employee &Employee::operator=(const Employee &e){
+    id = e.id;
+    name = e.name;
+    address = e.address;
+    hourWork = e.hourWork;
+    salaryPerHour = e.salaryPerHour;
+    workToDo = e.workToDo;
+    workDone = e.workDone;
+    return *this;
+}
+
+int Employee::getHourWork() const {
+    return hourWork;
+}
+
+void Employee::setHourWork(int hourWork) {
+    Employee::hourWork = hourWork;
+}
+
+int Employee::getSalaryPerHour() const {
+    return salaryPerHour;
+}
+
+void Employee::setSalaryPerHour(int salaryPerHour) {
+    Employee::salaryPerHour = salaryPerHour;
+}
+
+int Employee::getWorkToDo() const {
+    return workToDo;
+}
+
+void Employee::setWorkToDo(int workToDo) {
+    Employee::workToDo = workToDo;
+}
+
+int Employee::getWorkDone() const {
+    return workDone;
+}
+
+void Employee::setWorkDone(int workDone) {
+    Employee::workDone = workDone;
+}
+
+
--- Employee.h
+++ Employee.h
@@ -2,8 +2,50 @@
 #ifndef HW6_2_EMPLOYEE_H
 #define HW6_2_EMPLOYEE_H
 
+#include <iostream>
+#include "Person.h"
 
-class Employee {
+class Employee : public Person{
+
+    friend std::ostream &operator<<(std::ostream &os, const Employee &employee){
+        os << static_cast<const Person &>(employee) << " hourWork: " << employee.hourWork << " salaryPerHour: "
+           << employee.salaryPerHour << " workToDo: " << employee.workToDo << " workDone: " << employee.workDone;
+        return os;
+    }
+
+    friend std::istream &operator>>(std::istream &in , Employee &employee){
+        in>>employee.id>>employee.name>>employee.address>>employee.hourWork>>employee.salaryPerHour
+        >>employee.workToDo>>employee.workDone;
+        return in;
+    }
+
+
+private:
+    int hourWork , salaryPerHour , workToDo , workDone;
+public:
+    Employee() = default;
+
+    Employee(std::string id , std::string name , Address add , int hw , int sph , int wtd , int wd);
+
+    Employee(const Employee &cp);
+
+    Employee &operator=(const Employee&e);
+
+    int getHourWork() const;
+
+    void setHourWork(int hourWork);
+
+    int getSalaryPerHour() const;
+
+    void setSalaryPerHour(int salaryPerHour);
+
+    int getWorkToDo() const;
+
+    void setWorkToDo(int workToDo);
+
+    int getWorkDone() const;
+
+    void setWorkDone(int workDone);
 
 };
 
